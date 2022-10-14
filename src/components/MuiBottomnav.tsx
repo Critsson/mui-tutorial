f065600d4c5363023089527059e8aca5f8f3e969
Chris <chris.gao77@gmail.com>
2022-10-13 22:15:44 -0400

    Update

---
+++ src/components/MuiBottomnav.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { BottomNavigation, BottomNavigationAction } from '@mui/material'
+
+export const MuiBottomnav = () => {
+  return (
+    <BottomNavigation sx={{width:"100%", position:"absolute", bottom:"0"}}>
+        
+    </BottomNavigation>
+  )
+}
